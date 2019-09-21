59f0c31ec28bf5b73cb43cd192095ad5562001ce
solisandrew117 <solisandrew117@cs104u-2d.acad.fullerton.edu>
2019-09-21 14:57:13 -0700

    Rectangle

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,1 +1,35 @@
-// Random Rectangle
+// This program creates a Random Rectangle
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <iomanip>
+#include <ctime>
+#include <cstdlib>
+
+int main ()
+{
+
+//These are the maximum length/base, and height values for the rectangle.
+//    int max_length = 40;
+//    int max_width = 40;
+    int height = 3;
+
+    unsigned seed = time(0);
+    srand(seed);
+
+    int max = 40;
+    int num;
+    num = rand() % max + 1;
+
+    std::string eq;
+    eq.assign(num, '=');
+
+
+    std::cout << eq << std:: endl;
+    //std::cout << "The height is: " << height << std::endl;
+    std::cout << eq << std::endl;
+    std::cout << eq << std::endl;
+
+
+    return 0;
+}
